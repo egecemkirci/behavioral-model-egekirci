06e944c406fad9360455df0997153efb85af647a
egecemkirci <egecemkirci@gmail.com>
2018-11-22 15:27:31 +0100

    murmur hash added

--- targets/simple_switch/simple_switch.cpp
+++ targets/simple_switch/simple_switch.cpp
@@ -177,8 +177,57 @@ uint64_t md5_64(const char *initial_msg, size_t initial_len) {
     return answer;
 }
 
+#define ROT32(x, y) ((x << y) | (x >> (32 - y))) // avoid effort
+static inline uint32_t murmur3_32(const char *key, uint32_t len, uint32_t seed) {
+  static const uint32_t c1 = 0xcc9e2d51;
+  static const uint32_t c2 = 0x1b873593;
+  static const uint32_t r1 = 15;
+  static const uint32_t r2 = 13;
+  static const uint32_t m = 5;
+  static const uint32_t n = 0xe6546b64;
+  uint32_t hash = seed;
+  const int nblocks = len / 4;
+  const uint32_t *blocks = (const uint32_t *) key;
+  int i;
+  uint32_t k;
+  for (i = 0; i < nblocks; i++) {
+    k = blocks[i];
+    k *= c1;
+    k = ROT32(k, r1);
+    k *= c2;
+    hash ^= k;
+    hash = ROT32(hash, r2) * m + n;
+  }
+  const uint8_t *tail = (const uint8_t *) (key + nblocks * 4);
+  uint32_t k1 = 0;
+  switch (len & 3) {
+    case 3:
+      k1 ^= tail[2] << 16;
+    case 2:
+      k1 ^= tail[1] << 8;
+    case 1:
+      k1 ^= tail[0];
+      k1 *= c1;
+      k1 = ROT32(k1, r1);
+      k1 *= c2;
+      hash ^= k1;
+  }
+  hash ^= len;
+  hash ^= (hash >> 16);
+  hash *= 0x85ebca6b;
+  hash ^= (hash >> 13);
+  hash *= 0xc2b2ae35;
+  hash ^= (hash >> 16);
+  return hash;
+}
+
 namespace {
 
+struct my_murmur32 {
+  uint32_t operator()(const char *buf, size_t s) const {
+    return murmur3_32(buf, s, 1);
+  }
+};
 
 struct my_md5_64 {
   uint64_t operator()(const char *buf, size_t s) const {
@@ -216,6 +265,7 @@ struct bmv2_hash {
 REGISTER_HASH(my_md5_64);
 REGISTER_HASH(hash_ex);
 REGISTER_HASH(bmv2_hash);
+REGISTER_HASH(my_murmur32);
 
 extern int import_primitives();
 
